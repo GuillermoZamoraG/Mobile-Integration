ab051248a08034dfffac23160905d4ff0e3550e3
GuillermoZamoraG <65147462+GuillermoZamoraG@users.noreply.github.com>
2021-06-12 18:11:54 -0400

    Pantalla principal
    Se le añadió un pequeño tiempo de espera antes de que la app abra la pantalla principal.

---
+++ lib/home-screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Uber Eats"),
+        backgroundColor: Color(0xFF8335E8),
+      ),
+    );
+  }
+}
--- lib/start-screen.dart
+++ lib/start-screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home-screen.dart';
 
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _goToHome(context);
+
     return Scaffold(
       backgroundColor: Color(0xFF8335E8),
       body: Center(
@@ -13,5 +16,13 @@ class StartScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _goToHome(BuildContext context) {
+    Future.delayed(Duration(seconds: 1), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return HomeScreen();
+      }));
+    });
   }
 }
